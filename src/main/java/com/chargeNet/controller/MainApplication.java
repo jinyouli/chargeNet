b7a2982c8cffba2a18819750be43f94f9bc08aa1
jinyouli <229338934@qq.com>
2019-05-16 20:31:24 +0800

    tomcat可以打包
    tomcat可以打包

--- src/main/java/com/chargeNet/controller/MainApplication.java
+++ src/main/java/com/chargeNet/controller/MainApplication.java
@@ -1,10 +1,14 @@
 package com.chargeNet.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.ErrorPageFilter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
@@ -15,7 +19,7 @@ public class MainApplication extends SpringBootServletInitializer{
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
 		return app.sources(MainApplication.class);
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(MainApplication.class, args);
